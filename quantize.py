969fa89728cc458523d19fef3a1d6be61c913f84
LeviGibson <72767501+LeviGibson@users.noreply.github.com>
2022-03-27 12:01:16 -0400

    getting quantize.py to work

--- quantize.py
+++ quantize.py
@@ -5,6 +5,9 @@ from chess import Board
 import numpy as np
 from halfkp import flipPers
 import struct
+from math import pow
+
+np.set_printoptions(suppress=True)
 
 def load_params():
     weights, biases = [], []
@@ -23,7 +26,14 @@ def load_params():
 weights, biases = load_params()
 
 def activation(x):
+    x //= 127
     x[x < 0] = 0
+    x[x > 127] = 127
+    return x
+
+def f_activation(x):
+    x[x < 0] = 0
+    x[x > 1] = 1
     return x
 
 def sigmoid(x):
@@ -31,42 +41,78 @@ def sigmoid(x):
 
 def propogate(a):
     global weights, biases
-    for w,b in zip(weights,biases):
+
+    a = a.reshape(2, -1)
+    a = np.array([np.matmul(weights[0].T,a[0]) + biases[0], np.matmul(weights[0].T,a[1]) + biases[0]]).reshape((512,))
+    a[a < 0] = 0
+    a[a > 127] = 127
+    # print(a)
+
+    for w,b in zip(weights[1:],biases[1:]):
+        if b.shape == (1,):
+            a = ((np.matmul(w.T,a) + b) / 128) / 128
+            # a = a.astype(float) / 128
+            # print(a)
+            # a = (np.matmul(w.T,a) + b) / 128
+            # a = a[0]
+            # return 1/(1+pow(2.71828, -a))
+        else:
+            a = activation(np.matmul(w.T,a) + b)
+            print(a)
+            print()
+    return a
+
+def f_propogate(a):
+    global weights, biases
+
+    a = a.reshape(2, -1)
+    a = np.array([np.matmul(weights[0].T,a[0]) + biases[0], np.matmul(weights[0].T,a[1]) + biases[0]]).reshape((512,))
+    a = f_activation(a)
+    print(a)
+
+    for w,b in zip(weights[1:],biases[1:]):
         if b.shape == (1,):
             a = np.matmul(w.T,a) + b
+            a = a[0]
+            return 1/(1+pow(2.71828, -a))
         else:
-            a = activation(np.matmul(w.T,a))//64
-            # print(a)
+            a = f_activation(np.matmul(w.T,a) + b)
+            print(a)
+            print()
     return a
 
 for id, w in enumerate(weights):
-    weights[id] = (w*64).astype(int)
+    weights[id] = (w*127).astype(int)
 
 for id, b in enumerate(biases):
-    biases[id] = (b*64).astype(int)
+    biases[id] = (b*127).astype(int)
 
-# indicies = get_halfkp_indeicies(Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
-# ip = propogate(indicies)
-# print(ip)
+indicies = get_halfkp_indeicies(Board("k1n5/6pR/p1p1Rp2/2B3P1/2p5/P7/1PP5/1K6 w - - 3 42"))
+ip = propogate(indicies)
+print(ip)
 
-outfile = open("network.nnom", "wb")
-
-#print(weights[0])
-
-#for i in range(98304):
-#    for j in range(512):
-#        if weights[0][i][j]:print(i, j)
+outfile = open("network.nnue", "wb")
 
 for i in weights[0].flatten():
-    outfile.write(struct.pack('<h', round(i)))
+    outfile.write(struct.pack('<b', i))
 
-for i in weights[1].transpose().flatten():
-    outfile.write(struct.pack('<h', round(i)))
+for i in weights[1].flatten():
+    outfile.write(struct.pack('<b', i))
 
-for i in b[0].flatten():
-    outfile.write(struct.pack('<h', round(i)))
+for i in weights[2].flatten():
+    outfile.write(struct.pack('<b', i))
 
-b[1] *= 2
+for i in weights[3].flatten():
+    outfile.write(struct.pack('<b', i))
 
-for i in b[1].flatten():
-    outfile.write(struct.pack('<i', round(i)))
+for i in biases[0]:
+    outfile.write(struct.pack('<h', i))
+
+for i in biases[1]:
+    outfile.write(struct.pack('<i', i))
+
+for i in biases[2]:
+    outfile.write(struct.pack('<i', i))
+
+for i in biases[3]:
+    outfile.write(struct.pack('<i', i))
