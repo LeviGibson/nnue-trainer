d71cb49bf96d8c1a2b5db7b5f8313c61922dd20c
LeviGibson <72767501+LeviGibson@users.noreply.github.com>
2022-04-19 18:00:46 -0400

    added error catching in cleanPgn.py

--- Data/cleanPgn.py
+++ Data/cleanPgn.py
@@ -9,65 +9,67 @@ outfile = open("chessDataUnshuffled.csv", 'w')
 numFeatures = 0
 for lineId, line in enumerate(infile):
 
-    print(numFeatures)
-    if numFeatures > 41000000:
-        break
-    
-    line = line.replace('{', '')
-    line = line.replace('}', '')
-    line = line.replace(']', '')
-    line = line.replace('[', '')
-    line = line.replace('.', '')
-    line = line.replace('?', '')
-    line = line.replace('!', '')
-    line = line.replace('%eval ', '')
-    line = line.split('  ')
+    try:
+        if lineId % 1000 == 0:
+            print(numFeatures)
+        # if numFeatures > 41000000:
+        #     break
+        
+        line = line.replace('{', '')
+        line = line.replace('}', '')
+        line = line.replace(']', '')
+        line = line.replace('[', '')
+        line = line.replace('.', '')
+        line = line.replace('?', '')
+        line = line.replace('!', '')
+        line = line.replace('%eval ', '')
+        line = line.split('  ')
 
 
-    for id, obj in enumerate(line):
-        if 'clk' in obj:
-            line[id] = obj.split(' ')[0]
-        elif ' ' in obj:
-            line[id] = obj.split(' ')[1]
+        for id, obj in enumerate(line):
+            if 'clk' in obj:
+                line[id] = obj.split(' ')[0]
+            elif ' ' in obj:
+                line[id] = obj.split(' ')[1]
 
-    #lichess analysed games do not include the evaluation of the starting position
-    #so it is just added here (cp=0.20)
-    evals = [20]
-    moves = []
+        #lichess analysed games do not include the evaluation of the starting position
+        #so it is just added here (cp=0.20)
+        evals = [20]
+        moves = []
 
-    for obj in line:
-        try:
-            if obj[0] != '#':
-                int(obj)
-            evals.append(obj)
-        except:
-            moves.append(obj)
+        for obj in line:
+            try:
+                if obj[0] != '#':
+                    int(obj)
+                evals.append(obj)
+            except:
+                moves.append(obj)
 
-    #the position after the final move of the game is not looked at
-    #but it's included in lichess analyzed games
-    evals.pop()
+        #the position after the final move of the game is not looked at
+        #but it's included in lichess analyzed games
+        evals.pop()
 
-    board = chess.Board()
-    id = 0
-    padding = random.randint(0, 8)
+        board = chess.Board()
+        id = 0
+        padding = random.randint(0, 8)
 
-    for smove, eval in zip(moves, evals):
-        move = board.parse_san(smove)
+        for smove, eval in zip(moves, evals):
+            move = board.parse_san(smove)
 
-        if padding:
-            padding-=1
-            id+=1
+            if padding:
+                padding-=1
+                id+=1
+                board.push(move)
+                continue
+
+            if (not board.is_capture(move)) and (not board.gives_check(move)) and len(list(board.generate_pseudo_legal_captures())) < 4:
+                fen = board.fen()
+                padding = 8
+                outfile.write("{},{}\n".format(fen, str(eval)))
+                numFeatures+=1
+
             board.push(move)
-            continue
 
-        if (not board.is_capture(move)) and (not board.gives_check(move)) and len(list(board.generate_pseudo_legal_captures())) < 4:
-            fen = board.fen()
-            padding = 8
-            outfile.write("{},{}\n".format(fen, str(eval)))
-            numFeatures+=1
-            if numFeatures % 1000 == 0:print(numFeatures)
-
-        board.push(move)
-
-        id+=1
-
+            id+=1
+    except:
+        print("ERROR")
