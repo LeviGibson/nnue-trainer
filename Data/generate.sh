fce6d34bf4ac7c9b1fa9fbd0ea1fdf662b5f2578
LeviGibson <72767501+LeviGibson@users.noreply.github.com>
2022-05-01 14:47:45 -0400

    Update generate.sh

--- Data/generate.sh
+++ Data/generate.sh
@@ -8,27 +8,49 @@ rm data.pgn
 /opt/pypy3.8/bin/pypy3 ./cleanPgn.py
 shuf chessDataUnshuffled.csv >> 1.csv
 rm chessDataUnshuffled.csv
-rm all.txt
-cat *.csv >> all.txt
+rm chessData.csv
+cat *.csv >> chessData.csv
 
-# wget -O raw.pgn.bz2 https://database.lichess.org/standard/lichess_db_standard_rated_2022-02.pgn.bz2
-# pbzip2 -d raw.pgn.bz2 -v
-# mv *.pgn data.pgn
-# /opt/pypy3.8/bin/pypy3 ./findAnalysedGames.py
-# rm data.pgn
-# /opt/pypy3.8/bin/pypy3 ./cleanPgn.py
-# shuf chessDataUnshuffled.csv >> 1.csv
-# rm chessDataUnshuffled.csv
-# rm all.txt
-# cat *.csv >> all.txt
+wget -O raw.pgn.bz2 https://database.lichess.org/standard/lichess_db_standard_rated_2022-02.pgn.bz2
+pbzip2 -d raw.pgn.bz2 -v
+mv *.pgn data.pgn
+/opt/pypy3.8/bin/pypy3 ./findAnalysedGames.py
+rm data.pgn
+/opt/pypy3.8/bin/pypy3 ./cleanPgn.py
+shuf chessDataUnshuffled.csv >> 1.csv
+rm chessDataUnshuffled.csv
+rm chessData.csv
+cat *.csv >> chessData.csv
 
-# wget -O raw.pgn.bz2 https://database.lichess.org/standard/lichess_db_standard_rated_2022-01.pgn.bz2
-# pbzip2 -d raw.pgn.bz2 -v
-# mv *.pgn data.pgn
-# /opt/pypy3.8/bin/pypy3 ./findAnalysedGames.py
-# rm data.pgn
-# /opt/pypy3.8/bin/pypy3 ./cleanPgn.py
-# shuf chessDataUnshuffled.csv >> 1.csv
-# rm chessDataUnshuffled.csv
-# rm all.txt
-# cat *.csv >> all.txt
+wget -O raw.pgn.bz2 https://database.lichess.org/standard/lichess_db_standard_rated_2022-01.pgn.bz2
+pbzip2 -d raw.pgn.bz2 -v
+mv *.pgn data.pgn
+/opt/pypy3.8/bin/pypy3 ./findAnalysedGames.py
+rm data.pgn
+/opt/pypy3.8/bin/pypy3 ./cleanPgn.py
+shuf chessDataUnshuffled.csv >> 1.csv
+rm chessDataUnshuffled.csv
+rm chessData.csv
+cat *.csv >> chessData.csv
+
+wget -O raw.pgn.bz2 https://database.lichess.org/standard/lichess_db_standard_rated_2021-12.pgn.bz2
+pbzip2 -d raw.pgn.bz2 -v
+mv *.pgn data.pgn
+/opt/pypy3.8/bin/pypy3 ./findAnalysedGames.py
+rm data.pgn
+/opt/pypy3.8/bin/pypy3 ./cleanPgn.py
+shuf chessDataUnshuffled.csv >> 1.csv
+rm chessDataUnshuffled.csv
+rm chessData.csv
+cat *.csv >> chessData.csv
+
+wget -O raw.pgn.bz2 https://database.lichess.org/standard/lichess_db_standard_rated_2021-11.pgn.bz2
+pbzip2 -d raw.pgn.bz2 -v
+mv *.pgn data.pgn
+/opt/pypy3.8/bin/pypy3 ./findAnalysedGames.py
+rm data.pgn
+/opt/pypy3.8/bin/pypy3 ./cleanPgn.py
+shuf chessDataUnshuffled.csv >> 1.csv
+rm chessDataUnshuffled.csv
+rm chessData.csv
+cat *.csv >> chessData.csv
